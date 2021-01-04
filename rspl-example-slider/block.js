c8c9755d1849e31b196e65ea449218286b8cee64
dmitriyraspel <dmitriyraspel@yandex.ru>
2021-01-04 14:19:39 +0200

    add files

---
+++ rspl-example-slider/block.js
@@ -0,0 +1,43 @@
+( function( blocks, element, blockEditor ) {
+	var el = element.createElement;
+	var InnerBlocks = blockEditor.InnerBlocks;
+
+	blocks.registerBlockType( 'rspl-example/slider', {
+		title: 'Example: Slider',
+		icon: 'embed-photo', // Иконка Svg или Dashicons. https://developer.wordpress.org/resource/dashicons/.
+		category: 'layout',
+		keywords: [ 'slider', 'gallery', 'section' ],
+		supports: { align: ["wide","full"], default: 'full' },
+	
+		edit: function( props ) {
+			return el(
+				'div',
+				{ className: props.className },
+				el( InnerBlocks )
+			);
+		},
+	
+		save: function( props ) {
+			return el(
+				'div',
+				{ className: props.className },
+				el( InnerBlocks.Content )
+			);
+		},
+	} );
+} )
+(
+	window.wp.blocks,
+	window.wp.element,
+	window.wp.blockEditor 
+);
+/*
+"supports": {
+		"align": [ "wide", "full" ],
+		"anchor": true,
+		"__experimentalColor": {
+			"gradients": true,
+			"linkColor": true
+		}
+	}
+*/
